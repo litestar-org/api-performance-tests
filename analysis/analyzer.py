2c25dc6a719098b8f47d83c9a1dcd63829492d17
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-01-22 09:42:48 +0100

    updated analyzer

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,51 +1,60 @@
-import pandas as pd
 from pathlib import Path
+
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 results_dir = Path(".").parent / "results"
-
 df = pd.DataFrame()
 for file in results_dir.iterdir():
     if file.suffix == ".json":
         json = pd.read_json(file)
-        json["source"], json["type"] = file.name.split("-", 1)
-        json["filename"] = file.name
-        json["async"] = "async" in file.name
+        json["source"] = "fastapi" if "fastapi" in file.name else "starlite"
         df = df.append(json)
+
 
 # async comparison
 df2 = (
-    df.loc[df["async"] == True]
-    .groupby(["source", "type"])
-    .agg(mean_throughput=("throughput", "mean"))
+    df["url"]
+    .str.contains("async-square")
+    .groupby("source")
+    .agg(requests_processed=("2xx"))
 )
 df2 = df2.reset_index()
 plt.figure(figsize=(30, 10))
-plt1 = sns.barplot(x="type", y="mean_throughput", hue="source", data=df2)
-# plt1.set_xticklabels(plt1.get_xticklabels(), rotation=45)
-plt.savefig("analysis/output/async_comp.png")
+plt1 = sns.barplot(x="type", y="requests_processed", hue="source", data=df2)
+plt.savefig("analysis/output/async_square_requests.png")
 
 # square sync comparison
 squaredf = (
-    df.loc[(df["async"] == False) & (df["url"].str.contains("square"))]
-    .groupby(["source", "type"])
-    .agg(mean_throughput=("throughput", "mean"))
+    df["url"]
+    .str.contains("sync-square")
+    .groupby("source")
+    .agg(requests_processed=("2xx"))
 )
 squaredf = squaredf.reset_index()
 plt.figure(figsize=(30, 10))
-plt2 = sns.barplot(x="type", y="mean_throughput", hue="source", data=squaredf)
-# plt2.set_xticklabels(plt2.get_xticklabels(), rotation=45)
-plt.savefig("analysis/output/square_sync_comp.png")
+plt2 = sns.barplot(x="type", y="requests_processed", hue="source", data=squaredf)
+plt.savefig("analysis/output/sync_square_requests.png")
 
 # text sync comparison
-df3 = (
-    df.loc[(df["async"] == False) & (~df["url"].str.contains("square"))]
-    .groupby(["source", "type"])
-    .agg(mean_throughput=("throughput", "mean"))
+squaredf = (
+    df["url"]
+    .str.contains("plaintext")
+    .groupby("source")
+    .agg(requests_processed=("2xx"))
 )
-df3 = df3.reset_index()
+squaredf = squaredf.reset_index()
 plt.figure(figsize=(30, 10))
-plt3 = sns.barplot(x="type", y="mean_throughput", hue="source", data=df3)
-# plt3.set_xticklabels(plt3.get_xticklabels(), rotation=45)
-plt.savefig("analysis/output/text_sync_comp.png")
+plt2 = sns.barplot(x="type", y="requests_processed", hue="source", data=squaredf)
+plt.savefig("analysis/output/sync_plaintext_requests.png")
+
+
+# text sync comparison
+squaredf = (
+    df["url"].str.contains("json").groupby("source").agg(requests_processed=("2xx"))
+)
+squaredf = squaredf.reset_index()
+plt.figure(figsize=(30, 10))
+plt2 = sns.barplot(x="type", y="requests_processed", hue="source", data=squaredf)
+plt.savefig("analysis/output/sync_json_requests.png")
