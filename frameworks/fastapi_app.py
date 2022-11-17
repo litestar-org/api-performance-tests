e519e4345f7de56f325da4d648084b6a21e0af13
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-11-17 14:07:23 +0100

    Add starlette and fastapi tests

--- frameworks/fastapi_app.py
+++ frameworks/fastapi_app.py
@@ -1,97 +1,266 @@
-from asyncio import sleep
+from typing import TYPE_CHECKING
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import FileResponse, PlainTextResponse
+from fastapi.testclient import TestClient
+from starlette.status import HTTP_204_NO_CONTENT
+
+from . import data
+
+if TYPE_CHECKING:
+    from .test import EndpointSpec
 
 app = FastAPI()
 
 
-# json
+# plaintext response
 
 
-@app.get("/async-json-no-params")
-async def async_json_no_params() -> dict:
-    await sleep(0)
-    return {"message": "Hello, world!"}
+@app.get("/async-plaintext-6k", response_class=PlainTextResponse)
+async def async_plaintext_6k() -> str:
+    return data.TEXT_6k
 
 
-@app.get("/sync-json-no-params")
-def sync_json_no_params() -> dict:
-    return {"message": "Hello, world!"}
+@app.get("/sync-plaintext-6k", response_class=PlainTextResponse)
+def sync_plaintext_6k() -> str:
+    return data.TEXT_6k
 
 
-@app.get("/async-json/{first:str}")
-async def async_json_path_param(first: str) -> dict:
-    await sleep(0)
-    return {"message": first}
+@app.get("/async-plaintext-70k", response_class=PlainTextResponse)
+async def async_plaintext_70k() -> str:
+    return data.TEXT_70k
 
 
-@app.get("/sync-json/{first:str}")
-def sync_json_path_param(first: str) -> dict:
-    return {"message": first}
+@app.get("/sync-plaintext-70k", response_class=PlainTextResponse)
+def sync_plaintext_70k() -> str:
+    return data.TEXT_70k
 
 
-@app.get("/async-json-query-param")
-async def async_json_query_param(first: str) -> dict:
-    await sleep(0)
-    return {"message": first}
+# JSON response
 
 
-@app.get("/sync-json-query-param")
-def sync_json_query_param(first: str) -> dict:
-    return {"message": first}
+@app.get("/async-json-2k")
+async def async_json_2k() -> dict[str, str]:
+    return data.JSON_2K
 
 
-@app.get("/async-json-mixed-params/{second:str}")
-async def async_json_mixed_params(first: str, second: str) -> dict:
-    await sleep(0)
-    return {"message": first + second}
+@app.get("/sync-json-2k")
+def sync_json_2k() -> dict[str, str]:
+    return data.JSON_2K
 
 
-@app.get("/sync-json-mixed-params/{second:str}")
-def sync_json_mixed_params(first: str, second: str) -> dict:
-    return {"message": first + second}
+@app.get("/async-json-10k")
+async def async_json_10k() -> dict[str, str]:
+    return data.JSON_10K
 
 
-# plain text
+@app.get("/sync-json-10k")
+def sync_json_10k() -> dict[str, str]:
+    return data.JSON_10K
 
 
-@app.get("/asleep(ASYNC_SLEEP)")
-async def async_plaintext_no_params() -> str:
-    await sleep(0)
-    return "Hello, world!"
+@app.get("/async-json-450k")
+async def async_json_450k() -> dict[str, str]:
+    return data.JSON_450K
 
 
-@app.get("/sleep(ASYNC_SLEEP)")
-def sync_plaintext_no_params() -> str:
-    return "Hello, world!"
+@app.get("/sync-json-450k")
+def sync_json_450k() -> dict[str, str]:
+    return data.JSON_450K
 
 
-@app.get("/async-plaintext/{first:str}")
-async def async_plaintext_path_param(first: str) -> str:
-    await sleep(0)
-    return first
+# params
 
 
-@app.get("/sync-plaintext/{first:str}")
-def sync_plaintext_path_param(first: str) -> str:
-    return first
+@app.get("/async-no-params", status_code=HTTP_204_NO_CONTENT)
+async def async_no_params() -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.get("/async-plaintext-query-param")
-async def async_plaintext_query_param(first: str) -> str:
-    return first
+@app.get("/sync-no-params", status_code=HTTP_204_NO_CONTENT)
+def sync_no_params() -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.get("/sync-plaintext-query-param")
-def sync_plaintext_query_param(first: str) -> str:
-    return first
+@app.get("/async-path-params/{first:int}", status_code=HTTP_204_NO_CONTENT)
+async def async_path_params(first: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.get("/async-plaintext-mixed-params/{second:str}")
-async def async_plaintext_mixed_params(first: str, second: str) -> str:
-    return first + second
+@app.get("/sync-path-params/{first:int}", status_code=HTTP_204_NO_CONTENT)
+def sync_path_params(first: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.get("/sync-plaintext-mixed-params/{second:str}")
-def sync_plaintext_mixed_params(first: str, second: str) -> str:
-    return first + second
+@app.get("/async-query-param", status_code=HTTP_204_NO_CONTENT)
+async def async_query_params(first: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/sync-query-param", status_code=HTTP_204_NO_CONTENT)
+def sync_query_params(first: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/async-mixed-params/{second:int}", status_code=HTTP_204_NO_CONTENT)
+async def async_mixed_params(first: int, second: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/sync-mixed-params/{second:int}", status_code=HTTP_204_NO_CONTENT)
+def sync_mixed_params(first: int, second: int) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+# headers
+
+
+@app.get("/async-request-headers")
+async def async_request_headers(request: Request) -> Response:
+    header_dict = {}
+    for header_name, header_value in request.headers.items():
+        header_dict[header_name] = header_value
+    request.headers.getlist("header_1")
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/sync-request-headers")
+def sync_request_headers(request: Request) -> Response:
+    header_dict = {}
+    for header_name, header_value in request.headers.items():
+        header_dict[header_name] = header_value
+    request.headers.getlist("header_1")
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/async-response-headers")
+async def async_response_headers() -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT, headers=data.RESPONSE_HEADERS)
+
+
+@app.get("/sync-response-headers")
+def sync_response_headers() -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT, headers=data.RESPONSE_HEADERS)
+
+
+# cookies
+
+
+@app.get("/async-request-cookies")
+async def async_request_cookies(request: Request) -> Response:
+    cookie_dict = {}
+    for cookie_name, cookie_value in request.cookies.items():
+        cookie_dict[cookie_name] = cookie_value
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/sync-request-cookies")
+def sync_request_cookies(request: Request) -> Response:
+    cookie_dict = {}
+    for cookie_name, cookie_value in request.cookies.items():
+        cookie_dict[cookie_name] = cookie_value
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/async-response-cookies")
+async def async_response_cookies() -> Response:
+    res = Response(status_code=HTTP_204_NO_CONTENT)
+    for key, value in data.RESPONSE_COOKIES.items():
+        res.set_cookie(key, value)
+    return res
+
+
+@app.get("/sync-response-cookies")
+def sync_response_cookies() -> Response:
+    res = Response(status_code=HTTP_204_NO_CONTENT)
+    for key, value in data.RESPONSE_COOKIES.items():
+        res.set_cookie(key, value)
+    return res
+
+
+# url
+
+
+@app.route("/async-url-access")
+async def async_url_access(request: Request) -> None:
+    scheme = request.url.scheme
+    netloc = request.url.netloc
+    path = request.url.path
+    fragment = request.url.fragment
+    query = request.url.query
+    username = request.url.username
+    password = request.url.password
+    port = request.url.port
+    hostname = request.url.hostname
+    for param, value in request.query_params.items():
+        pass
+
+
+@app.route("/sync-url-access")
+def sync_url_access(request: Request) -> None:
+    scheme = request.url.scheme
+    netloc = request.url.netloc
+    path = request.url.path
+    fragment = request.url.fragment
+    query = request.url.query
+    username = request.url.username
+    password = request.url.password
+    port = request.url.port
+    hostname = request.url.hostname
+    for param, value in request.query_params.items():
+        pass
+
+
+# files
+
+
+@app.get("/async-file-response-100B")
+async def async_file_response_100b() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_100B, filename="response_file")
+
+
+@app.get("/async-file-response-50K")
+async def async_file_response_50k() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_50K, filename="response_file")
+
+
+@app.get("/async-file-response-1K")
+async def async_file_response_1k() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1K, filename="response_file")
+
+
+@app.get("/async-file-response-1M")
+async def async_file_response_1m() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1M, filename="response_file")
+
+
+@app.get("/sync-file-response-100B")
+def sync_file_response_100b() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_100B, filename="response_file")
+
+
+@app.get("/sync-file-response-50K")
+def sync_file_response_50k() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_50K, filename="response_file")
+
+
+@app.get("/sync-file-response-1K")
+def sync_file_response_1k() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1K, filename="response_file")
+
+
+@app.get("/sync-file-response-1M")
+def sync_file_response_1m() -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1M, filename="response_file")
+
+
+def run_spec_test(url: str, spec: "EndpointSpec") -> None:
+    with TestClient(app=app) as client:
+        res = client.get(url, **spec.get("request", {}))
+        assert res.status_code == spec["result"]["status_code"]
+        if expect_bytes := spec["result"].get("bytes"):
+            assert expect_bytes == res.content
+        if expect_text := spec["result"].get("text"):
+            assert expect_text == res.text
+        if expect_json := spec["result"].get("json"):
+            assert res.json() == expect_json
--- frameworks/starlette_app.py
+++ frameworks/starlette_app.py
@@ -1,107 +1,279 @@
-from asyncio import sleep
-from typing import Any
+from typing import TYPE_CHECKING
+from urllib.parse import parse_qsl
 
-import orjson
+
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import JSONResponse, PlainTextResponse
+from starlette.responses import FileResponse, Response, PlainTextResponse, JSONResponse
+from starlette.status import HTTP_204_NO_CONTENT
+from starlette.testclient import TestClient
+
+from . import data
+
+if TYPE_CHECKING:
+    from .test import EndpointSpec
 
 app = Starlette()
 
 # json
 
 
-class ORJSONResponse(JSONResponse):
-    def render(self, content: Any) -> bytes:
-        return orjson.dumps(content)
+# plaintext response
 
 
-@app.route(path="/async-json-no-params", methods=["GET"])
-async def async_json_no_params(_) -> ORJSONResponse:
-    await sleep(0)
-    return ORJSONResponse({"message": "Hello, world!"})
+@app.route("/async-plaintext-6k")
+async def async_plaintext_6k(request: Request) -> PlainTextResponse:
+    return PlainTextResponse(data.TEXT_6k)
 
 
-@app.route(path="/sync-json-no-params", methods=["GET"])
-def sync_json_no_params(_) -> ORJSONResponse:
-    return ORJSONResponse({"message": "Hello, world!"})
+@app.route("/sync-plaintext-6k")
+def sync_plaintext_6k(request: Request) -> PlainTextResponse:
+    return PlainTextResponse(data.TEXT_6k)
 
 
-@app.route(path="/async-json/{first:str}", methods=["GET"])
-async def async_json_path_param(request: Request) -> ORJSONResponse:
-    await sleep(0)
-    return ORJSONResponse({"message": request.path_params["first"]})
+@app.route("/async-plaintext-70k")
+async def async_plaintext_70k(request: Request) -> PlainTextResponse:
+    return PlainTextResponse(data.TEXT_70k)
 
 
-@app.route(path="/sync-json/{first:str}", methods=["GET"])
-def sync_json_path_param(request: Request) -> ORJSONResponse:
-    return ORJSONResponse({"message": request.path_params["first"]})
+@app.route("/sync-plaintext-70k")
+def sync_plaintext_70k(request: Request) -> PlainTextResponse:
+    return PlainTextResponse(data.TEXT_70k)
 
 
-@app.route(path="/async-json-query-param", methods=["GET"])
-async def async_json_query_param(request: Request) -> ORJSONResponse:
-    await sleep(0)
-    return ORJSONResponse({"message": request.query_params["first"]})
+# JSON response
 
 
-@app.route(path="/sync-json-query-param", methods=["GET"])
-def sync_json_query_param(request: Request) -> ORJSONResponse:
-    return ORJSONResponse({"message": request.query_params["first"]})
+@app.route("/async-json-2k")
+async def async_json_2k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_2K)
 
 
-@app.route(path="/async-json-mixed-params/{second:str}", methods=["GET"])
-async def async_json_mixed_params(request: Request) -> ORJSONResponse:
-    await sleep(0)
-    return ORJSONResponse({"message": request.query_params["first"] + request.path_params["second"]})
+@app.route("/sync-json-2k")
+def sync_json_2k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_2K)
 
 
-@app.route(path="/sync-json-mixed-params/{second:str}", methods=["GET"])
-def sync_json_mixed_params(request: Request) -> ORJSONResponse:
-    return ORJSONResponse({"message": request.query_params["first"] + request.path_params["second"]})
+@app.route("/async-json-10k")
+async def async_json_10k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_10K)
 
 
-# plain text
+@app.route("/sync-json-10k")
+def sync_json_10k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_10K)
 
 
-@app.route(path="/async-plaintext-no-params", methods=["GET"])
-async def async_plaintext_no_params(_) -> str:
-    await sleep(0)
-    return PlainTextResponse("Hello, world!")
+@app.route("/async-json-450k")
+async def async_json_450k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_450K)
 
 
-@app.route(path="/sync-plaintext-no-params", methods=["GET"])
-def sync_plaintext_no_params(_) -> str:
-    return PlainTextResponse("Hello, world!")
+@app.route("/sync-json-450k")
+def sync_json_450k(request: Request) -> JSONResponse:
+    return JSONResponse(data.JSON_450K)
 
 
-@app.route(path="/async-plaintext/{first:str}", methods=["GET"])
-async def async_plaintext_path_param(request: Request) -> str:
-    await sleep(0)
-    return PlainTextResponse(request.path_params["first"])
+# params
 
 
-@app.route(path="/sync-plaintext/{first:str}", methods=["GET"])
-def sync_plaintext_path_param(request: Request) -> str:
-    return PlainTextResponse(request.path_params["first"])
+@app.route("/async-no-params")
+async def async_no_params(request: Request) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.route(path="/async-plaintext-query-param", methods=["GET"])
-async def async_plaintext_query_param(request: Request) -> str:
-    await sleep()
-    return PlainTextResponse(request.query_params["first"])
+@app.route("/sync-no-params")
+def sync_no_params(request: Request) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.route(path="/sync-plaintext-query-param", methods=["GET"])
-def sync_plaintext_query_param(request: Request) -> str:
-    return PlainTextResponse(request.query_params["first"])
+@app.route("/async-path-params/{first:int}")
+async def async_path_params(request: Request) -> Response:
+    first = int(request.path_params["first"])
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.route(path="/async-plaintext-mixed-params/{second:str}", methods=["GET"])
-async def async_plaintext_mixed_params(request: Request) -> str:
-    await sleep(0)
-    return PlainTextResponse(request.query_params["first"] + request.path_params["second"])
+@app.route("/sync-path-params/{first:int}")
+def sync_path_params(request: Request) -> Response:
+    first = int(request.path_params["first"])
+    return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@app.route(path="/sync-plaintext-mixed-params/{second:str}", methods=["GET"])
-def sync_plaintext_mixed_params(request: Request) -> str:
-    return PlainTextResponse(request.query_params["first"] + request.path_params["second"])
+@app.route("/async-query-param")
+async def async_query_params(request: Request) -> Response:
+    first = int(request.query_params["first"])
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/sync-query-param")
+def sync_query_params(request: Request) -> Response:
+    first = int(request.query_params["first"])
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/async-mixed-params/{second:int}")
+async def async_mixed_params(request: Request) -> Response:
+    first = int(request.query_params["first"])
+    second = request.path_params["second"]
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/sync-mixed-params/{second:int}")
+def sync_mixed_params(request: Request) -> Response:
+    first = int(request.query_params["first"])
+    second = request.path_params["second"]
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+# headers
+
+
+@app.route("/async-request-headers")
+async def async_request_headers(request: Request) -> Response:
+    header_dict = {}
+    for header_name, header_value in request.headers.items():
+        header_dict[header_name] = header_value
+    request.headers.getlist("header_1")
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/sync-request-headers")
+def sync_request_headers(request: Request) -> Response:
+    header_dict = {}
+    for header_name, header_value in request.headers.items():
+        header_dict[header_name] = header_value
+    request.headers.getlist("header_1")
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/async-response-headers")
+async def async_response_headers(request: Request) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT, headers=data.RESPONSE_HEADERS)
+
+
+@app.route("/sync-response-headers")
+def sync_response_headers(request: Request) -> Response:
+    return Response(status_code=HTTP_204_NO_CONTENT, headers=data.RESPONSE_HEADERS)
+
+
+# cookies
+
+
+@app.route("/async-request-cookies")
+async def async_request_cookies(request: Request) -> Response:
+    cookie_dict = {}
+    for cookie_name, cookie_value in request.cookies.items():
+        cookie_dict[cookie_name] = cookie_value
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/sync-request-cookies")
+def sync_request_cookies(request: Request) -> Response:
+    cookie_dict = {}
+    for cookie_name, cookie_value in request.cookies.items():
+        cookie_dict[cookie_name] = cookie_value
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.route("/async-response-cookies")
+async def async_response_cookies(request: Request) -> Response:
+    res = Response(status_code=HTTP_204_NO_CONTENT)
+    for key, value in data.RESPONSE_COOKIES.items():
+        res.set_cookie(key, value)
+    return res
+
+
+@app.route("/sync-response-cookies")
+def sync_response_cookies(request: Request) -> Response:
+    res = Response(status_code=HTTP_204_NO_CONTENT)
+    for key, value in data.RESPONSE_COOKIES.items():
+        res.set_cookie(key, value)
+    return res
+
+
+# url
+
+
+@app.route("/async-url-access")
+async def async_url_access(request: Request) -> None:
+    scheme = request.url.scheme
+    netloc = request.url.netloc
+    path = request.url.path
+    fragment = request.url.fragment
+    query = request.url.query
+    username = request.url.username
+    password = request.url.password
+    port = request.url.port
+    hostname = request.url.hostname
+    for param, value in request.query_params.items():
+        pass
+
+
+@app.route("/sync-url-access")
+def sync_url_access(request: Request) -> None:
+    scheme = request.url.scheme
+    netloc = request.url.netloc
+    path = request.url.path
+    fragment = request.url.fragment
+    query = request.url.query
+    username = request.url.username
+    password = request.url.password
+    port = request.url.port
+    hostname = request.url.hostname
+    for param, value in request.query_params.items():
+        pass
+
+
+# files
+
+
+@app.route("/async-file-response-100B")
+async def async_file_response_100b(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_100B, filename="response_file")
+
+
+@app.route("/async-file-response-50K")
+async def async_file_response_50k(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_50K, filename="response_file")
+
+
+@app.route("/async-file-response-1K")
+async def async_file_response_1k(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1K, filename="response_file")
+
+
+@app.route("/async-file-response-1M")
+async def async_file_response_1m(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1M, filename="response_file")
+
+
+@app.route("/sync-file-response-100B")
+def sync_file_response_100b(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_100B, filename="response_file")
+
+
+@app.route("/sync-file-response-50K")
+def sync_file_response_50k(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_50K, filename="response_file")
+
+
+@app.route("/sync-file-response-1K")
+def sync_file_response_1k(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1K, filename="response_file")
+
+
+@app.route("/sync-file-response-1M")
+def sync_file_response_1m(request: Request) -> FileResponse:
+    return FileResponse(path=data.RESPONSE_FILE_1M, filename="response_file")
+
+
+def run_spec_test(url: str, spec: "EndpointSpec") -> None:
+    with TestClient(app=app) as client:
+        res = client.get(url, **spec.get("request", {}))
+        assert res.status_code == spec["result"]["status_code"]
+        if expect_bytes := spec["result"].get("bytes"):
+            assert expect_bytes == res.content
+        if expect_text := spec["result"].get("text"):
+            assert expect_text == res.text
+        if expect_json := spec["result"].get("json"):
+            assert res.json() == expect_json
